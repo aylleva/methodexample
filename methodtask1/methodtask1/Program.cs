1a04857bc85a1675172a19d43e9de82c55e3a82b
ayleva <ayllayeva@gmail.com>
2024-10-09 23:41:19 +0400

    initial commit

---
+++ methodtask1/methodtask1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.InteropServices;
+
+namespace methodtask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 6, 45, 3, 87, 1, 23,-87,  65, 100 };
+            Console.WriteLine(GetMin(nums));
+        }
+        public static int GetMin(int[] nums)
+        {
+            int min = nums[0]; 
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] < min)
+                {
+                    min = nums[i];
+                }
+                
+            }
+            return min;
+          
+        }
+    }
+}
---
+++ methodtask1/methodtask2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics.Contracts;
+using System.Reflection.Metadata.Ecma335;
+using System.Xml;
+
+namespace methodtask2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("cevrenin sahesi:");
+            Console.WriteLine(Area(4.11));
+
+            Console.WriteLine("duzbucaqlinin sahesi:");
+            Console.WriteLine(Area(2.6f,5.8f));
+
+            Console.WriteLine("paralepipedin sahesi:");
+            Console.WriteLine(Area(23,54,76));
+
+            Console.WriteLine("cevrenin sahesi:");
+            Console.WriteLine(Area(3,7,5,6.5));
+        }
+        public static double  Area(double r)//cevrenin sahesi
+        {
+             
+           double S = 0;
+            int p = 3;
+            S = p * r * r;
+
+            return S;
+        }
+       public static float Area(float a,float b)//duzbucaqlinin sahesi
+        {
+            float S = 0;
+            S = a * b;
+
+            return S;
+        }
+        public static double Area(double a,double b,double c)//paralepipedin tam sethinin sahesi
+        {
+            double S = 0;
+            S = 2 * (a * b + a * c + b * c);
+
+            return S;
+        }
+        public static double Area(double a, double b, double c, double r)
+        {
+            double S = 0;
+            double p = (a + b + c) / 2;
+            S = p * r;
+
+            return S;
+        }
+    }
+}
